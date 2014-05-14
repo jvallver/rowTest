2e8e7081542879c6363ae29f523a0c49af19d8c9
Jordi Vallverdu <jvallver@gmail.com>
2014-05-14 11:49:27 +0200

    change copyright text

--- rowTest.py
+++ rowTest.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 by Aivars Kalvans <jvallver@gmail.com>
+# Copyright (C) 2013 by Jordi Vallverdu <jvallver@gmail.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
